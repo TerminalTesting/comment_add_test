d014428454a183798e9aa5ef915232c41ff83d2f
TerminalTesting <testing@terminal.ru>
2014-09-29 13:02:08 +0600

    refactoring, add new test class method

--- comment_add_test.py
+++ comment_add_test.py
@@ -33,23 +33,62 @@ class CommentAddTest(unittest.TestCase):
 
     os.system('find -iname \*.png -delete')
 
+    def is_element_present(self, how, what, timeout=10):
+        """ Поиск элемента по локатору
+
+            По умолчанию таймаут 10 секунд, не влияет на скорость выполнения теста
+            если элемент найден, если нет - ждет его появления 10 сек
+            
+            Параметры:
+               how - метод поиска
+               what - локатор
+            Методы - атрибуты класса By:
+             |  CLASS_NAME = 'class name'
+             |  
+             |  CSS_SELECTOR = 'css selector'
+             |  
+             |  ID = 'id'
+             |  
+             |  LINK_TEXT = 'link text'
+             |  
+             |  NAME = 'name'
+             |  
+             |  PARTIAL_LINK_TEXT = 'partial link text'
+             |  
+             |  TAG_NAME = 'tag name'
+             |  
+             |  XPATH = 'xpath'
+                                             """
+	try:
+            return WebDriverWait(self.driver, timeout).until(EC.presence_of_element_located((how, what)))
+	except:
+            print u'Элемент не найден'
+	    print 'URL: ', self.driver.current_url
+	    print u'Метод поиска: ', how
+	    print u'Локатор: ', what
+	    screen_name = '%d.png' % int(time.time())
+	    self.driver.get_screenshot_as_file(screen_name)
+	    print u'Скриншот страницы: ', self.ARTSOURCE + screen_name
+	    raise Exception('ElementNotPresent')
+
     def tearDown(self):
         """Удаление переменных для всех тестов. Остановка приложения"""
         
         self.driver.close()
-        if sys.exc_info()[0]:   
+        if sys.exc_info()[0]:
+            print
             print sys.exc_info()[0]
 
     def test_comment_add(self):
         driver = self.driver
         comment_values = self.comment_values
+        element = self.is_element_present
         cnt=0
         
         driver.get('%slogin' % self.base_url)
-        time.sleep(5)
-        driver.find_element_by_id('username').send_keys(os.getenv('AUTH'))
-        driver.find_element_by_id('password').send_keys(os.getenv('AUTHPASS'))
-        driver.find_element_by_class_name('btn-primary').click()
+        element(By.ID, 'username', 5).send_keys(os.getenv('AUTH'))
+        element(By.ID, 'password', 5).send_keys(os.getenv('AUTHPASS'))
+        element(By.CLASS_NAME, 'btn-primary').click()
         time.sleep(5)
 
         #получения алиаса и id из БД
@@ -70,36 +109,26 @@ class CommentAddTest(unittest.TestCase):
         driver.get('%sproduct/%s/' % (self.base_url, good.alias))
 
         #Установка рейтинга 5 и отправка запроса с отзывом
-        driver.find_element_by_css_selector('#site_GoodsComment_form_rating_4 > a[title="5"]').click()
-        driver.find_element_by_id('site_GoodsComment_form_comment').send_keys(comment_values['comment'])
-        driver.find_element_by_css_selector('#writeComment > input.submitButton').click()
+        element(By.CSS_SELECTOR, '#site_GoodsComment_form_rating_4 > a[title="5"]').click()
+        element(By.ID, 'site_GoodsComment_form_comment').send_keys(comment_values['comment'])
+        element(By.CSS_SELECTOR, '#writeComment > input.submitButton').click()
 
         #Переход в админку и отет на отзыв из админки
         driver.get('%sterminal/admin/site/terminal/tgoodscomment/list' % self.base_url)
-        driver.find_element_by_link_text(comment_values['comment']).click()
-        Select(driver.find_element_by_css_selector('select[id*="_status"]')).select_by_visible_text(u'Активен на сайте')#активируем отзыв
-        driver.find_element_by_name('btn_update_and_edit').click()
+        element(By.LINK_TEXT, comment_values['comment']).click()
+        Select(element(By.CSS_SELECTOR, 'select[id*="_status"]')).select_by_visible_text(u'Активен на сайте')#активируем отзыв
+        element(By.NAME, 'btn_update_and_edit').click()
 
         #отвечаем на отзыв
-        driver.find_element_by_link_text(u'Новый ответ').click()
-        driver.find_element_by_css_selector('textarea[id*="_comment"]').send_keys(comment_values['admin_answer'])
-        driver.find_element_by_name('btn_create_and_edit').click()
-
-        #driver.close()
-        #driver = webdriver.Firefox()
-        #driver.get('%slogin' % self.base_url)
-        #time.sleep(5)
-        #driver.find_element_by_id('username').send_keys(os.getenv('AUTH'))
-        #driver.find_element_by_id('password').send_keys(os.getenv('AUTHPASS'))
-        #driver.find_element_by_class_name('btn-primary').click()
-        #time.sleep(5)
-
+        element(By.LINK_TEXT, u'Новый ответ').click()
+        element(By.CSS_SELECTOR, 'textarea[id*="_comment"]').send_keys(comment_values['admin_answer'])
+        element(By.NAME, 'btn_create_and_edit').click()
 
 
         #переходим в публичную часть и проверяем применился ли отзыв
         driver.get('%sproduct/%s/' % (self.base_url, good.alias))
         driver.refresh()
-        comment = driver.find_element_by_css_selector('#tabTarget7 div:first-child')
+        comment = element(By.CSS_SELECTOR, '#tabTarget7 div:first-child')
         
         #проверяем комментарий пользователя
         if comment.find_element_by_class_name('user_comment').text.strip() != comment_values['comment']:
@@ -119,13 +148,13 @@ class CommentAddTest(unittest.TestCase):
 
         #Отвечаем на отзыв от имени Терминал.ру из карточки товара
         comment.find_element_by_id('mother').click()
-        comment_answer_dialog = driver.find_element_by_class_name('commentAnswerDialog')
+        comment_answer_dialog = element(By.CLASS_NAME, 'commentAnswerDialog')
         comment_answer_dialog.find_element_by_id('site_GoodsCommentAnswer_form_comment').send_keys(comment_values['public_answer'])
         comment_answer_dialog.find_element_by_class_name('submitButton').click()
 
         #ждем пока страница перезагрузится и появится ответ на отзыв из карточки товара
         time.sleep(10)
-        comment = driver.find_element_by_css_selector('#tabTarget7 div:first-child')#обновляем данные, в кеше нет свежего отзыва
+        comment = element(By.CSS_SELECTOR, '#tabTarget7 div:first-child')#обновляем данные, в кеше нет свежего отзыва
 
         #проверяем ответ на комментарий пользователя из карточки товара
         if comment.find_elements_by_tag_name('div')[-1].find_elements_by_tag_name('p')[1].text.strip() != comment_values['public_answer']:
@@ -138,13 +167,13 @@ class CommentAddTest(unittest.TestCase):
             
         #удаляем 3 последних отзыва
         driver.get('%sterminal/admin/site/terminal/tgoodscomment/list' % self.base_url)
-        driver.find_element_by_name('idx[]').click()
-        driver.find_element_by_xpath('(//input[@name="idx[]"])[2]').click()
-        driver.find_element_by_xpath('(//input[@name="idx[]"])[3]').click()
+        element(By.NAME, 'idx[]').click()
+        element(By.XPATH, '(//input[@name="idx[]"])[2]').click()
+        element(By.XPATH, '(//input[@name="idx[]"])[3]').click()
         time.sleep(5)
-        Select(driver.find_element_by_name('action')).select_by_visible_text(u'Удалить')
-        driver.find_element_by_css_selector('div.actions.sonata-ba-list-actions > input.btn.btn-primary').click()
-        driver.find_element_by_css_selector('input.btn.btn-danger').click()
+        Select(element(By.NAME, 'action')).select_by_visible_text(u'Удалить')
+        element(By.CSS_SELECTOR, 'div.actions.sonata-ba-list-actions > input.btn.btn-primary').click()
+        element(By.CSS_SELECTOR, 'input.btn.btn-danger').click()
 
         for key in comment_values:
             try:
